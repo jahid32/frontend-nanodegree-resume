383621904f5499dd2b77a64f9e425101b147af83
Mostafizur Rahman <mostafizur.rahman@sourcetop.com>
2017-02-15 15:42:35 +0600

    update resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,69 @@
+var bio = {
+    'name' : 'Md Mostafizur Rahman',
+    'roll' : 'Web Developer',
+    'contacts' : {
+        'email' : 'jahid32@live.com',
+        'mobile' : '+8801717-176425',
+        'github' : 'https://github.com/jahid32',
+        'twitter' : '@mostafizur32',
+        'location' : 'Dhaka, Bangladesh'
+        },
+   
+    'biopic' : './images/mostafizur.jpg',
+    'welcomeMessage' : 'I am ambitious and self-motivated web developer with a strong technical background, have multiple years of web development experience. I have self-discipline and have the ability to work with the minimum of supervision.',
+    'skills' : ['Drupal','WordPress' , 'Laravel', 'HTML', 'SASS']
+}
+var works = {
+    'jobs' : [
+        {
+            'employer' : 'eSoftarena Ltd',
+            'title' : 'UI engineer',
+            'location' : 'banani, Dhaka',
+            'dates' : 'May 01, 2010 - September 30, 2011' ,
+            'description' : 'Responsible for participating in the front-end development cycle of new website development projects, and for implement and maintaining best practice procedures in all areas of work.' 
+        },
+        {
+            'employer' : 'Sourcetop Inc.',
+            'title' : 'Web developer',
+            'location' : 'Shajadpur, Gulshan Dhaka',
+            'dates' : 'July 15, 2012- continue' ,
+            'description' : 'Responsible for participating in the full life cycle of new website development projects, and for implement and maintaining best practice procedures in all areas of work.' 
+        }
+    ]
+};
+var educations = {
+    'schools': [
+        {
+            'name' : '',
+            'location' : '',
+            'degree_dates' : '',
+            'url' : '',
+            'majors' : '',
+        },
+        {
+            'name' : '',
+            'location' : '',
+            'degree_dates' : '',
+            'url' : '',
+            'majors' : '',
+        }
+    ],
+    'onlineCourses' : {
+        'title' : '',
+        'school' : '',
+        'dates' : '',
+        'url' : ''
+    }
+    
+};
+
+var projects = {
+    project : [
+        {
+            'title' : '',
+            'date' : '',
+            'description' : '',
+            'images' : []
+        }
+    ]
+};
